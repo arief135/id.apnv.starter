6d17c3db485a9319eda6f476d673e29c740515c2
Arief Pratama <ariefpratama135@gmail.com>
2024-04-27 21:13:01 +0700

    update

---
+++ webapp/controller/Content.controller.ts
@@ -0,0 +1,14 @@
+import JSONModel from "sap/ui/model/json/JSONModel";
+import BaseController from "./BaseController";
+
+/**
+ * @namespace id.apnv.starter.controller
+ */
+export default class Proxy extends BaseController {
+
+	private oModel: JSONModel;
+
+	public onInit() {
+	}
+
+}
--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,11 +1,39 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @namespace id.apnv.starter.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
+	public onInit() {
+		let oModel = new JSONModel()
+		oModel.setData({
+			"selectedKey": "page2",
+			"navigation": [
+				{
+					"title": "API Proxy",
+					"icon": "sap-icon://internet-browser",
+					"key": "proxy"
+				},
+				{
+					"title": "Users",
+					"icon": "sap-icon://person-placeholder",
+					"key": "users"
+				}
+			],
+			"fixedNavigation": [
+				{
+					"title": "Github",
+					"icon": "sap-icon://source-code"
+				},
+				{
+					"title": "Logoff",
+					"icon": "sap-icon://log",
+					"key": "logoff"
+				}
+			]
+		})
+		this.getView().setModel(oModel)
 	}
 }
